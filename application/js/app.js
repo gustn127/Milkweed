f9f9421da288ce41fbb5d17f0d03d67f90a9dd27
roshan158 <roshansaganti@hotmail.com>
2018-03-20 23:26:23 -0400

    3/20/18

---
+++ application/js/app.js
@@ -0,0 +1,13 @@
+/*
+ *	IRC Chrome Extension
+ *	Developers: Roshan Saganti, Hynsoo Kim
+ *	Date: March 20th, 2018
+ */
+
+$(document).ready(function()
+{
+	$('#clickBtn').click(function()
+	{
+		$('#test').append("test");
+	});
+});
